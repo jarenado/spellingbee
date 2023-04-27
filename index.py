1b7b0349b67db3059dd2a1e9821daef91df03d9f
Jose Arenado <vidalstat@gmail.com>
2023-04-27 01:39:38 -0400

    chore: add todos

--- index.py
+++ index.py
@@ -2,9 +2,11 @@ from bs4 import BeautifulSoup
 import json
 file_path = "./test.json"
 
+# TODO: move this to its own function (fetch daily puzzle)
 with open("sb.html") as fp:
     soup = BeautifulSoup(fp, "html.parser")
 
+# TODO: write json to file
 with open(file=file_path, mode='r') as read_file:
     object = json.load(read_file)
     pretty = json.dumps(object, indent=2)
